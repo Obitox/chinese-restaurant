907190114c65f61d4151fb33eb07d2aab6c5352e
Bojan Miric <bojan0071@hotmail.com>
2019-07-28 01:07:32 +0200

    + fontawesome, + checkboxes working(cleanup required)

--- restaurant-app-fe/src/components/ItemDialog.jsx
+++ restaurant-app-fe/src/components/ItemDialog.jsx
@@ -6,16 +6,21 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAllergies, faCheck } from '@fortawesome/free-solid-svg-icons'
 
 export default class ItemDialog extends React.Component {
-//   const [open, setOpen] = React.useState(false);
-
   constructor(props){
     super(props);
     // let arrayOfIngredients = [];
     this.state = {
-        
+        checkboxes: []
     };
+
+    this.getCheckboxState = this.getCheckboxState.bind(this);
+    this.handleIngredientCheck = this.handleIngredientCheck.bind(this);
   }
 
   componentDidMount = () => {
@@ -31,43 +36,64 @@ export default class ItemDialog extends React.Component {
   }
 
   initCheckBoxes = (ingredient) => {
-    console.log('ID: ' + ingredient.IngredientID)
-    this.setState({[ingredient.IngredientID]: false});
-    console.log(this.state);
+    let checkbox = {
+      key: ingredient.IngredientID,
+      value: false
+    };
+    let temp = this.state.checkboxes;
+    temp.push(checkbox);
+    this.setState({['checkboxes']: temp})
   }
 
-  // handleIngredientCheck = (obj) => {
-  //     this.setState({['ingredients']: })
-  // }
+  getCheckboxState(key){
+    this.state.checkboxes.filter(function(checkbox){
+      if(checkbox.key == key){
+        return checkbox.value;
+      }
+    });
+    return false;
+  }
 
-//   function handleClickOpen() {
-//     setOpen(true);
-//   }
-
-//   function handleClose() {
-//     setOpen(false);
-//   }
+  handleIngredientCheck(event, key){
+      let checkboxes = this.state.checkboxes;
+      let updatedCheckboxes = [];
+      for(var i =0; i < checkboxes.length; i++){
+        if(checkboxes[i].key == key){
+          checkboxes[i].value = event.target.checked;
+        }
+        updatedCheckboxes.push(checkboxes[i]);
+      }
+      this.setState({['checkboxes']: updatedCheckboxes})
+  }
 
   render(){
-    // let ingredients = this.props.object[0].Ingredient.map((obj, key) => 
-    //             <Checkbox
-    //               key={obj.IngredientID}
-    //               checked={obj.checked}
-    //               onLoad={() => this.initCheckboxes(oj)}
-    //               onChange={() => this.handleIngredientCheck(obj)}
-    //               value="checkedA"
-    //               inputProps={{
-    //                 'aria-label': 'primary checkbox',
-    //               }}
-    //           />
-    // );
+    let ingredients = this.props.object[0].Ingredient.map((obj, index) => 
+                                        <FormControlLabel
+                                          key={index}
+                                          control={
+                                            <Checkbox
+                                              key={obj.IngredientID}
+                                              checked={this.state.checkboxes.filter(checkbox => checkbox.key == obj.IngredientID).value} 
+                                              onChange={(event) => this.handleIngredientCheck(event,  obj.IngredientID)} 
+                                              value={obj.Title} />
+                                          }
+                                          label={obj.Title}
+                                        />
+    );
 
+    let ingredientsWithAllergens = this.props.object[0].Ingredient.filter(ingredient => ingredient.Allergens !== "");
+    let allergens;
+    if(ingredientsWithAllergens.length > 0){
+      allergens = ingredientsWithAllergens.map(ingredient => ingredient.Allergens);
+    }
+    console.log("Allergens: " + allergens)
     return (
         <div>
           <Dialog open={this.props.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
                     <DialogTitle id="form-dialog-title">{this.props.object[0].Item.Title}</DialogTitle>
                     <DialogContent>
-                    
+                    {ingredients}
+                    {allergens !== undefined ? <div><FontAwesomeIcon icon={faAllergies} />{allergens}</div> : <div><FontAwesomeIcon icon={faCheck} />No allergens :)</div>}
                     <DialogContentText>
                         To subscribe to this website, please enter your email address here. We will send updates
                         occasionally.
